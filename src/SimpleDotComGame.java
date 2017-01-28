b2b61a93fb9da4373758e0534781d8101a5540f1
Alexsandr Prokhorov <kikimiki163@gmail.com>
2017-01-28 20:55:07 +0400

    initi3 commit

--- src/SimpleDotComGame.java
+++ src/SimpleDotComGame.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by aleksandr on 22.01.17.
  */
@@ -9,12 +11,20 @@ public class SimpleDotComGame {
 
 
 
-        SimpleDotCom theDotCom = new SimpleDotCom();
+        DotCom theDotCom = new DotCom();
         int randomNum = (int) (Math.random() * 5);
 
-        int[] locations = {randomNum, randomNum+1, randomNum+2};
-        theDotCom.setLocationCells(locations);
-        boolean isAlive = true;
+        ArrayList<String> myList = new ArrayList<String>();
+        String a = new String("randomNum");
+        myList.add(a);
+        String b = new String("randomNum+1");
+        myList.add(b);
+        String c = new String("randomNum+2");
+        myList.add(c);
+
+        theDotCom.setLocationCells(myList);
+
+        boolean isAlive =true;
 
         while (isAlive == true) {
             String guess = helper.getUserInput("Введите число");
